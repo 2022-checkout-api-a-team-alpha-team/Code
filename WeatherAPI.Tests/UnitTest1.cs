c941d40474ddee7bf022c2e28310c913f04d1ec4
shahzaib199579 <shahzaib199579@gmail.com>
2022-10-19 15:44:09 +0100

    Initial Commit with Project Template.

---
+++ WeatherAPI.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace WeatherAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
