313405cdf2f953794843fbd3d0633b7162babfaa
Prasanna <prasannadevi@gmail.com>
2022-10-23 01:26:40 +0100

    Modified Program.cs file Added Services Scope with IWeatherService and WeatherService

--- WeatherAPI/Helper/ConstantsHelper.cs
+++ WeatherAPI/Helper/ConstantsHelper.cs
@@ -1,7 +1,10 @@
-﻿namespace WeatherAPI.Helper
+﻿using static System.Net.WebRequestMethods;
+
+namespace WeatherAPI.Helper
 {
     public static class ConstantsHelper
     {
-        public const string GEO_API_URL = "https://geocoding-api.open-meteo.com/v1/search?name=[city]&count=1"; 
+        public const string GEO_API_URL = "https://geocoding-api.open-meteo.com/v1/search?name=[city]&count=1";
+        public const string WEATHER_API_URL = "https://api.open-meteo.com/v1/forecast?latitude=[latitude]&longitude=[longitude]&hourly=temperature_2m";
     }
 }
--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IGeoService, GeoService>();
+builder.Services.AddScoped<IWeatherService, WeatherService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
