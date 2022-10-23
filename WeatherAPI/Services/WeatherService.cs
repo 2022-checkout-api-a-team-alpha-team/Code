fcd1ec6916c979d2018c252c565a4c18ba231b5d
Prasanna <prasannadevi@gmail.com>
2022-10-23 01:28:33 +0100

    Added WeatherService.cs file Added Methods to get hourly temperature from Latitude_Longitude and Cityname

--- WeatherAPI/Services/WeatherService.cs
+++ WeatherAPI/Services/WeatherService.cs
@@ -1,6 +1,43 @@
-﻿namespace WeatherAPI.Services
+﻿using System.Text.Json;
+using WeatherAPI.DTOs;
+using WeatherAPI.Helper;
+
+namespace WeatherAPI.Services
 {
-    public class WeatherService
+    public class WeatherService : IWeatherService
     {
+        private HttpClient _httpClient;
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<GetHourlyTemperatureResponseDTO> GetHourlyTemperatureByLatitudeAndLongitude(double latitude, double longitude)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                AllowTrailingCommas = true,
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+            };
+            var result = await _httpClient.GetFromJsonAsync<GetHourlyTemperatureResponseDTO>(ConstantsHelper.WEATHER_API_URL.Replace("[latitude]",latitude.ToString().Trim()).Replace("[longitude]", longitude.ToString().Trim()), options);
+            return result!;
+        }
+
+        public async Task<GetHourlyTemperatureResponseDTO> GetHourlyTemperatureByCity(string cityName)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                AllowTrailingCommas = true,
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var GeoCoordinates = await _httpClient.GetFromJsonAsync<GetGeoCoordResponseDTO>(ConstantsHelper.GEO_API_URL.Replace("[city]", cityName), options);
+            double latitude = GeoCoordinates!.Results.ToList()[0].Latitude;
+            double longitude = GeoCoordinates.Results.ToList()[0].Longitude;
+
+            var result = await _httpClient.GetFromJsonAsync<GetHourlyTemperatureResponseDTO>(ConstantsHelper.WEATHER_API_URL.Replace("[latitude]", latitude.ToString().Trim()).Replace("[longitude]", longitude.ToString().Trim()), options);
+            return result!;
+        }
     }
 }
