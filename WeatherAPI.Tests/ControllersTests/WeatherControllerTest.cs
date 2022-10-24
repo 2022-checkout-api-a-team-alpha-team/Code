f09577c3f56e468dc61fc9ca9c2928690a747dd4
jsabithabanu <jsabithabanu@gmail.com>
2022-10-24 23:30:16 +0100

    Added the Setup for WeatherController Mock service in WeatherControllerTest.cs - To include tests for the weather controller - Using mock service of IWeatherService

--- WeatherAPI.Tests/ControllersTests/WeatherControllerTest.cs
+++ WeatherAPI.Tests/ControllersTests/WeatherControllerTest.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Text.Json;
+using WeatherAPI.Controllers;
+using WeatherAPI.DTOs;
+using WeatherAPI.Helper;
+using WeatherAPI.Services;
+using WeatherAPI.Models;
 
 namespace WeatherAPI.Tests.ControllersTests
-{
-    internal class WeatherControllerTest
+{    
+    public class WeatherControllerTest
     {
+        private WeatherController _weatherController;
+        private Mock<IWeatherService> _mockWeatherService;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange           
+            _mockWeatherService = new Mock<IWeatherService>();
+            _weatherController = new WeatherController(_mockWeatherService.Object);
+        }      
     }
 }
