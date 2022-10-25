118537f4c8bca16220136bc3e5d500fb9d4382c5
Prasanna <prasannadevi@gmail.com>
2022-10-25 22:15:51 +0100

    Moved HourlyTempForeCastAndSuggestionsDTO.cs to DTOs folder

---
+++ WeatherAPI/DTOs/HourlyTempForeCastAndSuggestionsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherAPI.DTOs
+{
+    public class HourlyTempForeCastAndSuggestionsDTO
+    {
+        public int Day { get; set; }
+        public string? Date { get; set; }
+        public double? AverargeTemperature { get; set; }
+
+        public string? Suggestion { get; set; }
+    }
+}
